c27c30f32af001b3a857dd98f5f903a7a7af9d2e
saroNERO <62933463+saroNERO@users.noreply.github.com>
2020-10-28 09:30:46 +0100

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,3 @@
+<?php
+$conn=new mysqli('remotemysql.com','PDsWaSTZR9','6Xi9F82obS','PDsWaSTZR9');
+?>
--- insert.php
+++ insert.php
@@ -1,5 +1,5 @@
 <?php
-$conn=new mysqli('remotemysql.com','PDsWaSTZR9','6Xi9F82obS','PDsWaSTZR9');
+require('connect.php');
 $autor = $_POST['nazwisko'];
 $tytul = $_POST['tytul'];
 $sql1 = "INSERT INTO `lib_autor`(`id_autor`, `autor`) VALUES (NULL, '$autor')";
@@ -7,4 +7,6 @@ $sql2 = "INSERT INTO `lib_tytul`(`id_tytul`, `tytul`) VALUES (NULL, '$tytul')";
 $conn->query($sql1);
 $conn->query($sql2);
 mysqli_close($conn);
+// header('Location: http://bartek-saracen-crud.herokuapp.com/');
+echo("działa to ?");
 ?>
