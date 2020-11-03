45eea008d29df278ae5afb2c0de446bcd8fc99af
saroNERO <62933463+saroNERO@users.noreply.github.com>
2020-11-03 11:24:19 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -5,13 +5,13 @@ $tytul = $_POST['tytul'];
 $sql1 = "INSERT INTO `lib_autor`(`id_autor`, `autor`) VALUES (NULL, '$autor')";
 if ($conn->query($sql1) === TRUE) {
     $last_id1 = $conn->insert_id;
-    echo "New record created successfully. Last inserted ID is: " . $last_id1;
   }
 $sql2 = "INSERT INTO `lib_tytul`(`id_tytul`, `tytul`) VALUES (NULL, '$tytul')";
 if ($conn->query($sql2) === TRUE) {
     $last_id2 = $conn->insert_id;
-    echo "New record created successfully. Last inserted ID is: " . $last_id2;
   }
+$sql3 = "INSERT INTO `lib_autor_tytul`(`id_autor_tytul`, `id_autor`, `id_tytul`) VALUES (NULL,$last_id1,$last_id2)";
+$conn->query($sql3);
 mysqli_close($conn);
-//header('Location: http://bartek-saracen-crud.herokuapp.com/');
+header('Location: http://bartek-saracen-crud.herokuapp.com/');
 ?>
