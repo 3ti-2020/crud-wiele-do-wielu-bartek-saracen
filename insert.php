71ccb7da2d8a63910f20c3ff7ed3bcd2437fed33
= <=>
2020-11-03 11:06:22 +0100

    random

--- insert.php
+++ insert.php
@@ -3,7 +3,15 @@ require('connect.php');
 $autor = $_POST['nazwisko'];
 $tytul = $_POST['tytul'];
 $sql1 = "INSERT INTO `lib_autor`(`id_autor`, `autor`) VALUES (NULL, '$autor')";
+if ($conn->query($sql1) === TRUE) {
+    $last_id1 = $conn->insert_id;
+    echo "New record created successfully. Last inserted ID is: " . $last_id1;
+  }
 $sql2 = "INSERT INTO `lib_tytul`(`id_tytul`, `tytul`) VALUES (NULL, '$tytul')";
+if ($conn->query($sql2) === TRUE) {
+    $last_id2 = $conn->insert_id;
+    echo "New record created successfully. Last inserted ID is: " . $last_id2;
+  }
 $conn->query($sql1);
 $conn->query($sql2);
 mysqli_close($conn);
