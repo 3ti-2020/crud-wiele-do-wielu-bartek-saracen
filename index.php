be1be5fdff85f400ac03e41b57dff7d816b384ef
= <=>
2020-11-04 09:19:40 +0100

    new

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bartosz Saracen gr 2</title>
     <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="style1.css">
 </head>
 <body>
     <div class="container">
@@ -15,7 +14,7 @@
     <nav>
         <ul>
             <li><a href="#">link</a></li>
-            <li><a href="#">link</a></li>
+            <li><a href="cards/index.html">Cards</a></li>
             <li><a href="#">link</a></li>
         </ul>
     </nav>
@@ -35,36 +34,6 @@
         </div>
     </div>
     <main>
-    <div class="card">
-        <a href="#" class="forimg"><img src="images/model.png" class="image" alt="model"></a>
-        <div class="bg-for-text">
-            <div class="item1 item">
-                <div class="items1 items">
-                    <div class="fol">
-                        <span class="followers">1000</span>
-                        <a class="follow">Follow</a>
-                    </div>
-                </div>
-                <div class="items2 items"></div>
-            </div>
-            <div class="item2 item">
-                <div class="n1 n">
-                    <h3 class="h3n1">Patrick Wood</h3>
-                </div>
-                <div class="n2 n">
-                    <h3 class="h3n2">CEO / Co-Founder</h3>
-                </div>
-            </div>
-            <div class="item3 item">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A nam incidunt omnis maiores obcaecati aut deleniti sunt totam veritatis quibusdam eaque, reiciendis eveniet? A veniam neque placeat incidunt iusto doloribus.</p>
-            </div>
-            <div class="item4 item">
-                <a class="icon1 icons" href="#"><img src="https://www.flaticon.com/svg/static/icons/svg/145/145808.svg" alt="Pinterest"></a>
-                <a class="icon2 icons" href="#"><img src="https://www.flaticon.com/svg/static/icons/svg/145/145812.svg" alt="Twitter"></a>
-                <a class="icon3 icons" href="#"><img src="https://www.flaticon.com/svg/static/icons/svg/145/145801.svg" alt="Dribbble"></a>
-            </div>
-        </div>
-    </div>
     <?php
     require('connect.php');
     $conn->set_charset('utf8');
@@ -89,6 +58,5 @@ HTML;
     </main>
     <footer></footer>
     </div>
-    <script src="script.js"></script>
 </body>
 </html>
--- script.js
+++
@@ -1,6 +0,0 @@
-    const followers = document.querySelector('.followers');
-    const follow = document.querySelector('.follow');
-    follow.addEventListener('click',() => {
-        followers.innerHTML==="1000" ? followers.innerHTML="1001" : followers.innerHTML="1000";
-        follow.innerHTML==="Follow" ? follow.innerHTML="Unfollow" : follow.innerHTML="Follow";
-    });
