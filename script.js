9931dc5e5322d93f2cb778919177366a7c33ffab
= <=>
2020-11-05 13:26:21 +0100

    new

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const modal = document.querySelector(".modal");
+const container = document.querySelector(".container");
 document.getElementById("signin").addEventListener('click',() => {
     modal.style.display="flex";
+    container.style.display="none";
 });
 document.querySelector(".close").addEventListener('click',() => {
     modal.style.display="none";
+    container.style.display="flex";
 });
