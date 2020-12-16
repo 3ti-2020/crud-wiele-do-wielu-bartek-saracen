d2cc4ac45b8f5c291de81d0faff5862368d374b2
saroNERO <62933463+saroNERO@users.noreply.github.com>
2020-12-16 09:38:27 +0100

    blog update

--- blog/index.php
+++ blog/index.php
@@ -1,6 +1,5 @@
 <?php
                 require_once('../connect.php');
-                $post=$conn->query("SELECT `id` as idpost,`name`,`post` FROM post");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -17,25 +16,33 @@
         </nav>
         <main>
         <?php
+                if(isset($_GET['action'])){
+                    $gettag = $_GET['action'];
+                    $post = $conn->query("SELECT `post`.`id`, `name`, `post` FROM `post_tag`, `post`, `tag` WHERE `post_tag`.`post_id` = `post`.`id` AND `post_tag`.`tag_id` = `tag`.`id` AND `tag` = '$gettag'"); 
+                }else{
+                    $post = $conn->query("SELECT `id`,`name`, `post` FROM `post`");
+                }
                 while($row=$post->fetch_assoc()){
-                   echo <<<HTML
-                    <div class="POST">
-                    <div>
-                        <h1 class="h1__post">$row[name]</h1>
-HTML;
-                        $postid=$row['idpost'];
-                        $tag=$conn->query("SELECT `tag` FROM `tag`,`post_tag`,`post` where post_tag.tag_id=tag.id AND post_tag.post_id=post.id AND post.id=$postid");
-                        while($row1=$tag->fetch_assoc()){
-                            echo <<<HTML
-                        <a href='#' class="tag">$row1[tag]</a>
-HTML;
-                    }
                     echo <<<HTML
-                    </div>
-                    <div>
-                        <p class="p__post">$row[post]</p>
-                    </div>
-                    </div>
+                        <div class="POST">
+                            <div>
+                                <h1 class="h1__post">$row[name]</h1>
+                                <div class="tags">
+HTML;
+                                    $posts = $row["id"];
+                                    $tag = $conn->query("SELECT `tag` FROM `post_tag`, `post`, `tag` WHERE `post_tag`.`post_id` = `post`.`id` AND `post_tag`.`tag_id` = `tag`.`id` AND `post_tag`.`post_id` = $posts");
+                                    while($row1=$tag->fetch_assoc()){
+                                        echo <<<HTML
+                                        <a class='tag' href='?action=$row1[tag]'>$row1[tag],</a>
+HTML;
+                                    }
+                    echo <<<HTML
+                                </div>
+                            </div>
+                            <div class="posts">
+                                <p class="p__post">$row[post]</p>
+                            </div>
+                        </div>
 HTML;
                 }
 
